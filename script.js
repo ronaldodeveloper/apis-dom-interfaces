c271eb3727553eabcc9a66012cba60ba556f9901
Ronaldo Mariano <rgm.frontend@hotmail.com>
2022-04-13 20:32:52 -0300

    APIs DOM

---
+++ script.js
@@ -0,0 +1,17 @@
+const log= console.log
+/*
+   APIs DOM - propriedades e métodos...   
+     _________Funcionalidades_________
+*/
+
+// adicionar / inserir
+// criar 
+// verificar se contém / existe
+// editar / alterar/ modificar 
+// selecionar / obter
+// aplicar estilos
+// manipular eventos
+// mover
+// remover
+// substituir
+
